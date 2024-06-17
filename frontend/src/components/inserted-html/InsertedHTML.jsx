e952131621b733c2ed99f3baa2ad25acb358e17e
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-06-17 09:30:42 +0300

    InsertedHTML styling

--- frontend/src/components/inserted-html/InsertedHTML.jsx
+++ frontend/src/components/inserted-html/InsertedHTML.jsx
@@ -2,6 +2,6 @@ import './insertedHTML.css'
 
 export default function InsertedHTML({ html }){
     return (
-        <div className="container inserted-html-container" dangerouslySetInnerHTML={{ __html: html }} />
+        <div className="container inserted-html-container m-top" dangerouslySetInnerHTML={{ __html: html }} />
     );
 }
