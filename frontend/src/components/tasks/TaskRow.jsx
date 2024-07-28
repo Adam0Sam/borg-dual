7cd31b995b29687eaba40d7e6d05a6e6c6ded97c
Adam Samulionis <adamsam@wix.com>
2024-07-29 00:27:43 +0300

    Fixed task row container link to absolute

--- frontend/src/components/tasks/TaskRow.jsx
+++ frontend/src/components/tasks/TaskRow.jsx
@@ -2,26 +2,21 @@ import { NavLink } from "react-router-dom";
 import { getStrapiURL } from "../../utils/api";
 
 export default function TaskRow({ taskImages }) {
-    console.log(taskImages)
-    return (
-        <div className="task-cover-container">
-            {
-                taskImages.map((image) => {
-                    const imageUrl = image.attributes?.url || '';
-                    return (
-                        <NavLink
-                            to="task-examples"
-                            key={image.id}
-                            className='task-cover'>
-                            <img
-                                src={getStrapiURL(imageUrl)}
-                                alt={image.attributes.name}
-                                key={image.attributes.name}>
-                            </img>
-                        </NavLink>
-                    )
-                })
-            }
-        </div>
-    )
+  console.log(taskImages);
+  return (
+    <div className="task-cover-container">
+      {taskImages.map((image) => {
+        const imageUrl = image.attributes?.url || "";
+        return (
+          <NavLink to="/task-examples" key={image.id} className="task-cover">
+            <img
+              src={getStrapiURL(imageUrl)}
+              alt={image.attributes.name}
+              key={image.attributes.name}
+            ></img>
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
