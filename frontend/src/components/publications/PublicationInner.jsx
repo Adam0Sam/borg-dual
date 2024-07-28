cda108547a89e9611b28c4e68dd9e1b1bc9585ed
Adam Samulionis <adamsam@wix.com>
2024-07-29 00:24:51 +0300

    All publications display button

--- frontend/src/components/publications/PublicationInner.jsx
+++ frontend/src/components/publications/PublicationInner.jsx
@@ -1,37 +1,62 @@
-import { useState, useEffect, useRef } from 'react';
-import RichText from '../rich-text/RichText';
+import { useState, useEffect } from "react";
+import RichText from "../rich-text/RichText";
 
 export default function PublicationInner({ publicationLinks }) {
-    const [publicationContent, setPublicationContent] = useState([]);
-    const buttonRef = useRef(null);
-    const [activeButtonIndex, setActiveButtonIndex] = useState(0);
+  const [publicationContent, setPublicationContent] = useState([]);
+  const [activeButtonIndex, setActiveButtonIndex] = useState(0);
 
-    useEffect(() => {
-        if (buttonRef.current) {
-            buttonRef.current.click();
+  useEffect(() => {
+    setPublicationContent(
+      publicationLinks[0].publication.data.attributes.RichText
+    );
+    setActiveButtonIndex(0);
+  }, [publicationLinks]);
+
+  const handleClick = (text, index) => {
+    setPublicationContent(text);
+    setActiveButtonIndex(index);
+  };
+
+  return (
+    <>
+      <div
+        className={`publication__buttons inner-buttons ${
+          publicationLinks.length > 1 ? "visible" : ""
+        } `}
+      >
+        {publicationLinks.map((link, index) => (
+          <button
+            onClick={() => {
+              handleClick(link.publication.data.attributes.RichText, index);
+              console.log(link.publication.data.attributes.RichText);
+            }}
+            key={link.name}
+            className={`pub-button inner-button ${
+              index === activeButtonIndex ? "active" : ""
+            }`}
+          >
+            {link.name}
+          </button>
+        ))}
+        {
+          <button
+            onClick={() => {
+              handleClick(
+                publicationLinks
+                  .map((link) => link.publication.data.attributes.RichText)
+                  .flat(),
+                publicationLinks.length
+              );
+            }}
+            className={`pub-button inner-button ${
+              publicationLinks.length === activeButtonIndex ? "active" : ""
+            }`}
+          >
+            All
+          </button>
         }
-    }, [publicationLinks])
-
-    const handleClick = (text, index) => {
-        setPublicationContent(text);
-        setActiveButtonIndex(index);
-    }
-
-    return (
-        <>
-            <div className={`publication__buttons inner-buttons ${publicationLinks.length > 1 ? "visible" : ""} `}>
-                {publicationLinks.map((link, index) =>
-                    <button
-                        ref={index === 0 ? buttonRef : null}
-                        onClick={() => handleClick(link.publication.data.attributes.RichText, index)}
-                        key={link.name}
-                        className={`pub-button inner-button ${index === activeButtonIndex ? "active" : ""}`}>
-                        {link.name}
-                    </button>
-                )}
-            </div>
-            <RichText text={publicationContent} noPadding/>
-        </>
-    )
-
+      </div>
+      <RichText text={publicationContent} noPadding />
+    </>
+  );
 }
